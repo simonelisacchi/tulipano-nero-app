8aeaf2b778385c3deb078a27b30b71b8bddd0e7d
simonelisacchi <simone.lisacchi2000@gmail.com>
2026-08-23 12:34:45 +0200

    V-3.5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker Tulipano Nero
 // Mette in cache l'app (HTML/CSS/JS/icone) così si apre e funziona anche senza internet.
-const CACHE_VERSION = 'tulipano-nero-v40';
+const CACHE_VERSION = 'tulipano-nero-v41';
 const APP_SHELL = [
   './',
   './index.html',
